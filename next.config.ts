ac4e9a284867d972eecb6bc2c92f2af8d7302b7e
5hvn5vk3 <kusami0907@yahoo.co.jp>
2025-10-22 12:02:33 +0900

    fix: Next.js Cannary最新版に設定を合わせた

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    cacheComponents: true,
+    experimental: {
+        dynamicIO: true,
+    },
 };
 
 export default nextConfig;
